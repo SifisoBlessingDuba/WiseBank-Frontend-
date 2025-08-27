a44f53f53b8bc4f77514acb171552cea1661f49e
Laeeqah-Arendse <161837984+Laeeqah-Arendse@users.noreply.github.com>
2025-08-27 19:09:18 +0200

    Login, Sign-up & Password pages

---
+++ lib/signup_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'Profile.dart';
+import 'change_password_page.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final nameController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Sign Up")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Image.asset("assets/logo.png", height: 120),
+              const SizedBox(height: 20),
+
+              const Text(
+                "Create Account",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                    labelText: "Full Name", border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 15),
+
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                    labelText: "Email", border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 15),
+
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: "Password", border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Profile()),
+                  );
+                },
+                child: const Text("Sign Up"),
+              ),
+              const SizedBox(height: 15),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
