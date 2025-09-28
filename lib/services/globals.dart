003898a47af2cd0d167c3bf7e00e9eb73941a3b6
Itumeleng <i.bedesho@plum.systems>
2025-09-28 20:45:33 +0200

    This is my changes done transaction reads and updates the database

--- lib/services/globals.dart
+++ lib/services/globals.dart
@@ -2,4 +2,5 @@ library my_app.globals;
 
 String loggedInUserId = "";
 
-String apiBaseUrl = "http://10.127.2.123:8081";
+//String apiBaseUrl = "http://localhost:8081";//
+const String apiBaseUrl = "http://10.0.2.2:8081";
